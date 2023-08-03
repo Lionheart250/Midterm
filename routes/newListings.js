22b1b42fd6b90a293d9b9b256aa9adde84de1088
k7yusuf <K7yusuf@gmail.com>
2023-08-03 14:15:28 -0400

    added to newListing

--- routes/newListings.js
+++ routes/newListings.js
@@ -3,21 +3,16 @@ const router = express.Router();
 
 // Middleware function to check if the user is logged in
 const requireAuth = (req, res, next) => {
-  if (!req.session || !req.session.user) {
+  if (!req.session || !req.session.user_id) {
     return res.status(401).json({ error: 'You must be logged in to perform this action.' });
   }
   // User is authenticated, proceed to the next middleware or route handler
   next();
 };
 
-// The route handler for /users
-router.get('/', (req, res) => {
-  // Access the dbPool from req object
-  const dbPool = req.dbPool;
 // Route handler for creating a new listing
-router.post('/', async (req, res) => {
+router.post('/', requireAuth, async (req, res) => {
   try {
-    // Access the dbPool from req object
     const dbPool = req.dbPool;
 
     // Get the user's ID from the session (assuming you have implemented authentication)
@@ -39,23 +34,6 @@ router.post('/', async (req, res) => {
   } catch (error) {
     console.error('Error creating new listing:', error);
     res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-// The route handler for creating new listings
-router.post('/newListings', requireAuth, async (req, res) => {
-  try {
-    const { title, description, price, imageUrl } = req.body;
-    // Validate the data if needed
-
-    // Call the createListing function to insert the new listing into the database
-    const newListing = await createListing(title, description, price, imageUrl);
-
-    // Return the newly created listing as the response
-    res.status(201).json(newListing);
-  } catch (error) {
-    console.error('Error creating new listing:', error);
-    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
